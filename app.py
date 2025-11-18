2eb89df5e776454b37441e75547d45c76f0b2f6a
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-11-18 02:20:54 +0200

    update

--- app.py
+++ app.py
@@ -18,18 +18,6 @@ def index():  # put application's code here
 
     cards = soup.select(".card-frame")
 
-    # for card in cards:
-    #     title = card.select_one(".card-title h6").get_text(strip=True)
-    #     print(title)
-    #     exit()
-
-    #
-    # children = select[0].findChildren("option", recursive=False)
-    # date = children[0].text.strip()
-    #
-    # tables = soup.findChildren('table')
-    # my_table = tables[0]
-    # rows = my_table.findChildren(['th', 'tr'])
     total_rows = []
 
     keys = [
@@ -67,13 +55,7 @@ def index():  # put application's code here
             'orario': hours,
             'katastasi': status
         }
-        #     index = 0
-        #     for cell in cells:
-        #         if index in [0, 1]:
-        #             index += 1
-        #             continue
-        #         final_row[keys[index]] = cell.text.strip()
-        #         index += 1
+
         total_rows.append(final_row)
     #
     pharmacies = []
@@ -83,57 +65,6 @@ def index():  # put application's code here
             districts.append(farmakeio_row['perioxi'])
         pharmacies.append(farmakeio_row)
     return {'date': date, 'districts': districts, 'pharmacies': pharmacies}
-    # response = requests.get('https://fsa-efimeries.gr/')
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    #
-    # select = soup.findChildren('select', {'id': 'Date'})
-    #
-    # children = select[0].findChildren("option", recursive=False)
-    # date = children[0].text.strip()
-    #
-    # tables = soup.findChildren('table')
-    # my_table = tables[0]
-    # rows = my_table.findChildren(['th', 'tr'])
-    # total_rows = []
-    # keys = [
-    #     '-',
-    #     '-',
-    #     'perioxi',
-    #     'farmakeio',
-    #     'dieuthinsi',
-    #     'tilefono',
-    #     'orario',
-    #     'katastasi'
-    # ]
-    # for row in rows:
-    #     cells = row.findChildren('td')
-    #     if len(cells) == 0:
-    #         continue
-    #     final_row = {
-    #         'perioxi': '',
-    #         'farmakeio': '',
-    #         'dieuthinsi': '',
-    #         'tilefono': '',
-    #         'orario': '',
-    #         'katastasi': ''
-    #     }
-    #     index = 0
-    #     for cell in cells:
-    #         if index in [0, 1]:
-    #             index += 1
-    #             continue
-    #         final_row[keys[index]] = cell.text.strip()
-    #         index += 1
-    #     total_rows.append(final_row)
-    #
-    # pharmacies = []
-    # districts = []
-    # for farmakeio_row in total_rows:
-    #     if farmakeio_row['perioxi'] not in districts:
-    #         districts.append(farmakeio_row['perioxi'])
-    #     pharmacies.append(farmakeio_row)
-    #
-    # return {'date': date, 'districts': districts, 'pharmacies': pharmacies}
 
 
 if __name__ == '__main__':
